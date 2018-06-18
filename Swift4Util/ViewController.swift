b1c272f577c8e6be43aeaa47372a36d6a73aa282
rudifa <rudi.farkas@gmail.com>
2018-06-18 14:36:03 +0200

    Initial Commit

---
+++ Swift4Util/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift4Util
+//
+//  Created by Rudolf Farkas on 18.06.18.
+//  Copyright © 2018 Rudolf Farkas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
