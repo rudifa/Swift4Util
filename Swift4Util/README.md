346b811422a0311c198c5cec08cd87e87b291a66
rudifa <rudi.farkas@gmail.com>
2018-06-18 16:20:17 +0200

    add RegexUtil with unit tests

--- Swift4Util/README.md
+++ Swift4Util/README.md
@@ -3,7 +3,7 @@
 Start of a collection of swift 4 utilities, with unit tests.
 
 ### DateUtil
-Extends class ```Date``` with properties  
+Extends class ```Date``` with properties:  
 ```
 .ddMMyyyy
 .HHmmss
@@ -15,4 +15,44 @@ Usage example:
 print("Today", date.ddMMyyyy, "at", date.HHmmss, "...")
 
 Today 18.06.2018 at 15:02:57 ...
+```
+
+### RegexUtil
+
+Extends class ```String``` with methods:  
+```
+.matches(for regex: String) -> [String]
+.isBlank() -> Bool
+.doesMatch(regex: String) -> Bool
+.extractUUID() -> String
+```
+
+Usage examples:
+```
+var string = "🇩🇪€4€9"
+var matched = string.matches(for: "[0-9]")
+XCTAssertEqual(matched, ["4", "9"])
+XCTAssertFalse(string.isBlank())
+```
+```
+var string = "🇩🇪€4€9"
+var matched = string.matches(for: "[0-9]")
+//print(matched)
+XCTAssertEqual(matched, ["4", "9"])
+print("---", string, matched, "blank: \(string.isBlank())")
+XCTAssertFalse(string.isBlank())
+```
+```
+string = " \t  \n"
+XCTAssertTrue(string.isBlank())
+```
+``
+var string = "🇩🇪€4€9"
+XCTAssertTrue(string.doesMatch(regex: "[0-9]"))
+XCTAssertTrue(string.doesMatch(regex: "€"))
+```
+```
+let contains_UUID = "https://stick-scan/product_images%2F08D41FB1-8B2E-4F6F-977A-BFA876AEF775.png"
+let extracted_UUID = contains_UUID.extractUUID()
+XCTAssertEqual(extracted_UUID, "08D41FB1-8B2E-4F6F-977A-BFA876AEF775")
 ```
---
+++ Util/RegexUtil.swift
@@ -0,0 +1,56 @@
+//
+//  RegexUtil.swift version 0.4
+//  Swift4Util
+//
+//  Created by Rudolf Farkas on 28.04.18.
+//  Copyright © 2018 Rudolf Farkas. All rights reserved.
+//
+
+import Foundation
+
+extension String
+{
+    // see
+    //https://learnappmaking.com/regular-expressions-swift-string/
+    //https://stackoverflow.com/questions/27880650/swift-extract-regex-matches
+    
+    // returns an array of substrings in self that matched the regex
+    func matches(for regex: String) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: self, range: NSRange(self.startIndex..., in: self))
+            return results.map {
+                String(self[Range($0.range, in: self)!])
+            }
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    // returns true if self is blank (empty or consists of whitespace characters only)
+    func isBlank() -> Bool {
+        return self.matches(for: "^\\s*$") != []
+    }
+    
+    // returns true if self matches the pattern
+    func doesMatch(regex: String) -> Bool {
+        return self.matches(for: regex) != []
+    }
+    
+}
+
+extension String
+{
+    // Return UUID (if any) found in self (for diagnostics)
+    // ex: "https://stick-scan/product_images%2F08D41FB1-8B2E-4F6F-977A-BFA876AEF775.png"
+    func extractUUID() -> String {
+        let matches = self.matches(for: "[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}")
+        if matches.count > 0 {
+            return matches[0]
+        }
+        return ""
+    }
+
+}
+
