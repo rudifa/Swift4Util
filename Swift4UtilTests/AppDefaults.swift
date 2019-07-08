383e859d8b5e58e8263dd8cea580026d13a273ed
rudifa <rudi.farkas@gmail.com>
2019-07-08 16:43:35 +0200

    add doc comments for AppDefaults

--- Swift4UtilTests/AppDefaults.swift
+++ Swift4UtilTests/AppDefaults.swift
@@ -8,11 +8,20 @@
 
 import Foundation
 
-class AppDefaults<Struct: Codable> {
-    static func data(for key: String) -> Data? {
+/// Utility for saving to and restoring from UserDefaults.standard arbitrary structs
+struct AppDefaults<Struct: Codable> {
+    /// Look up data for the setting key
+    ///
+    /// - Parameter key: setting key
+    /// - Returns: setting data value (nil if absent from UserDefaults)
+    private static func data(for key: String) -> Data? {
         return UserDefaults.standard.object(forKey: key) as? Data
     }
 
+    /// Look up the value for the setting key in
+    ///
+    /// - Parameter key: setting key
+    /// - Returns: setting value (nil if absent from UserDefaults.standard)
     static func setting(for key: String) -> Struct? {
         if let data = UserDefaults.standard.data(forKey: key) {
             if let prefLang = try? Struct.decode(from: data) {
@@ -22,6 +31,12 @@ class AppDefaults<Struct: Codable> {
         return nil
     }
 
+    /// Save the value in UserDefaults.standard
+    ///
+    /// - Parameters:
+    ///   - value: to save in UserDefaults
+    ///   - key:  setting key
+    /// - Returns: true if successful
     static func set(_ value: Struct, forKey key: String) -> Bool {
         if let encoded = try? value.encode() {
             UserDefaults.standard.set(encoded, forKey: key)
